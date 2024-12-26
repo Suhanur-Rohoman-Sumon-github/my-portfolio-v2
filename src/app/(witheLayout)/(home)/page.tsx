d76dcbef02207f30e56e5cb1c806d4a359802896
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2024-12-26 23:34:49 +0600

    some more problem solved

--- src/app/(witheLayout)/(home)/page.tsx
+++ src/app/(witheLayout)/(home)/page.tsx
@@ -7,14 +7,18 @@ import MyBlogs from "@/components/home/MyBlogs";
 import Project from "@/components/home/MyProjects";
 
 import Skills from "@/components/home/Skills";
-
+import dynamic from "next/dynamic";
+const AboutComponent = dynamic(() => import("@/components/home/About"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
 import React from "react";
 
 const page = () => {
   return (
     <div>
       <Banner />
-      <About />
+      <AboutComponent />
       <Skills />
       <Expariance />
       <Project />

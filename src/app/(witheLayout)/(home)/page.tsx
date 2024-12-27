74ed47300639cfdaded826ea5836590718134854
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2024-12-27 09:24:01 +0600

    some more change

--- src/app/(witheLayout)/(home)/page.tsx
+++ src/app/(witheLayout)/(home)/page.tsx
@@ -6,18 +6,14 @@ import MyBlogs from "@/components/home/MyBlogs";
 import Project from "@/components/home/MyProjects";
 
 import Skills from "@/components/home/Skills";
-import dynamic from "next/dynamic";
-const AboutComponent = dynamic(() => import("@/components/home/About"), {
-  loading: () => <p>Loading...</p>,
-  ssr: false,
-});
+
 import React from "react";
 
 const page = () => {
   return (
     <div>
       <Banner />
-      <AboutComponent />
+      {/* <AboutComponent /> */}
       <Skills />
       <Expariance />
       <Project />
